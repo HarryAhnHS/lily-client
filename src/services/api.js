7b8f85d9cc3d72a929629ab0fe57786b74309f7e
Hongsuh Ahn <hongsuhahn@gmail.com>
2025-04-08 08:33:17 -0700

    deploy: rebuild with api url logs

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const authorizedFetch = async (url, token, options = {}) => {
+  console.log("Fetching from:", `${API_URL}${url}`);
   return fetch(`${API_URL}${url}`, {
     ...options,
     headers: {
